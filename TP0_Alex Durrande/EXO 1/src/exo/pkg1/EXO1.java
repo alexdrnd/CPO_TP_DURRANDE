990cdf418ad4b984ff064e2ca244d5a2720adac5
Alexandre Durrande <alexandre.durrande@epfedu.fr>
2023-09-18 15:20:11 +0200

    Fini tP 1 Partie 1

---
+++ TP0_Alex Durrande/EXO 1/src/exo/pkg1/EXO1.java
@@ -0,0 +1,33 @@
+ /*
+Alexandre Durrande
+FG2 TDA
+Familiarisé avec Netbeans 
+18/09/2023
+*/
+
+package exo.pkg1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Alexandre
+ */
+public class EXO1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+      
+        System.out.println("Au revoir");
+        // TODO code application logic here
+        String prenom;
+        Scanner sc;
+sc = new Scanner(System.in);
+System.out.println("Bonjour, quel est votre prenom ?");
+sc.nextLine();
+}
+
+    }
