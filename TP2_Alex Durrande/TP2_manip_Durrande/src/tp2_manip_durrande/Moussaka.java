183d35affd2ec93cec2f85d0ba92374b6cc49fb5
Alexandre Durrande <alexandre.durrande@epfedu.fr>
2023-10-11 11:04:23 +0200

    a

---
+++ TP2_Alex Durrande/TP2_manip_Durrande/src/tp2_manip_durrande/Moussaka.java
@@ -0,0 +1,21 @@
+package tp2_manip_durrande;
+
+/*
+Alexandre Durrande
+FG2 TDA
+TP2 
+18/09/2023
+ */
+
+/**
+ *
+ * @author Alexandre
+ */
+public class Moussaka {
+    
+    int nbCalories;
+    public Moussaka(int nbCalories) {
+        this.nbCalories = nbCalories;
+    
+}
+}
