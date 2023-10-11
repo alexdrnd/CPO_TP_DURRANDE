d93ed819db21351800d1b8bbd25a0371e70afcd9
Alexandre Durrande <alexandre.durrande@epfedu.fr>
2023-10-11 11:04:32 +0200

    a

---
+++ TP2_Alex Durrande/TP2_manip_Durrande/src/tp2_manip_durrande/Tartiflette.java
@@ -0,0 +1,20 @@
+package tp2_manip_durrande;
+
+/*
+Alexandre Durrande
+FG2 TDA
+TP2 
+18/09/2023
+ */
+
+/**
+ *
+ * @author Alexandre
+ */
+public class Tartiflette {
+    int nbCalories;
+    public Tartiflette(int nbCalories) {
+          this.nbCalories = nbCalories;
+    }
+    
+}
