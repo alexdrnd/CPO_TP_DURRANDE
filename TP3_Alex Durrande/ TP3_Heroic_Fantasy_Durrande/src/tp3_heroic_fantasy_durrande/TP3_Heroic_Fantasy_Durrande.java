5e11eda023f79f4d54178737921dc81f0d60d0a1
Alexandre Durrande <alexandre.durrande@epfedu.fr>
2023-10-23 16:24:43 +0200

    .....

--- TP3_Alex Durrande/ TP3_Heroic_Fantasy_Durrande/src/tp3_heroic_fantasy_durrande/TP3_Heroic_Fantasy_Durrande.java
+++ TP3_Alex Durrande/ TP3_Heroic_Fantasy_Durrande/src/tp3_heroic_fantasy_durrande/TP3_Heroic_Fantasy_Durrande.java
@@ -6,15 +6,13 @@ TP3
  */
 package tp3_heroic_fantasy_durrande;
 
-
 import Armes.Baton;
 import Personnages.Magicien;
 import Personnages.Guerrier;
 import Personnages.Personnages;
 import Armes.Arme;
 import Armes.Epee;
-import java.util.Vector;
-
+import java.util.ArrayList;
 
 /**
  *
@@ -32,27 +30,34 @@ public class TP3_Heroic_Fantasy_Durrande {
         Arme bat1 = new Baton("Chêne", 4, 5);
         Arme bat2 = new Baton("Charme", 5, 6);
 
-        Vector tabArmes = new Vector();
-        tabArmes.add(Epee1);
-        tabArmes.add(Epee2);
-        tabArmes.add(bat1);
-        tabArmes.add(bat2);
-        for (int i = 0; i < tabArmes.size(); i++) {
-            System.out.println(tabArmes.elementAt(i));
-        }
+
+// ...
+
+ArrayList<Arme> tabArmes = new ArrayList<Arme>();
+tabArmes.add(Epee1);
+tabArmes.add(Epee2);
+tabArmes.add(bat1);
+tabArmes.add(bat2);
+
+for (int i = 0; i < tabArmes.size(); i++) {
+    System.out.println(tabArmes.get(i));
+}
 
         Personnages Magicien1 = new Magicien("Gandalf", 65, true);
         Personnages Magicien2 = new Magicien("Garcimore", 44, false);
         Personnages Guerrier1 = new Guerrier("Conan", 78, false);
         Personnages Guerrier2 = new Guerrier("Lannister", 45, true);
-        Vector tabPerso = new Vector();
-        tabPerso.add(Magicien1);
-        tabPerso.add(Magicien2);
-        tabPerso.add(Guerrier1);
-        tabPerso.add(Guerrier2);
-        for (int i = 0; i < tabPerso.size(); i++) {
-            System.out.println(tabPerso.elementAt(i));
-        }
+
+ArrayList<Personnages> tabPerso = new ArrayList<Personnages>();
+tabPerso.add(Magicien1);
+tabPerso.add(Magicien2);
+tabPerso.add(Guerrier1);
+tabPerso.add(Guerrier2);
+
+for (int i = 0; i < tabPerso.size(); i++) {
+    System.out.println(tabPerso.get(i));
+}
+
 
     }
 
