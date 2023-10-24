e2cc5ea9fa2acbb53e573a12ec28b4c546b07df0
Alexandre Durrande <alexandre.durrande@epfedu.fr>
2023-10-24 14:53:41 +0200

    .....

--- TP3_Alex Durrande/ TP3_Heroic_Fantasy_Durrande/src/Personnages/Guerrier.java
+++ TP3_Alex Durrande/ TP3_Heroic_Fantasy_Durrande/src/Personnages/Guerrier.java
@@ -12,18 +12,32 @@ import Personnages.*;
  *
  * @author Alexandre
  */
-public class Guerrier extends Personnages {
-    private boolean cheval;
+public class Guerrier extends Personnages{
     
-    public Guerrier(String nom, int niveauDeVie, boolean cheval) {
-        this.nom = nom;
-        this.niveauDeVie = niveauDeVie;
-        this.cheval = cheval;
+    boolean cheval;    
+    
+    public Guerrier (String n, int pv, boolean a){
+        super(n , pv);
+        cheval=a;
+        
     }
+     @Override
+public String toString() {
+String chaine_a_retourner;
+if (cheval == true){
+chaine_a_retourner = ("Le Guerrier "+nom+ " a "+niveauDeVie+ "point de vie et est à cheval et mani "+armeEnMain);
+return chaine_a_retourner;
+}
+else {
+    chaine_a_retourner = ("Le Guerrier "+nom+ " a "+niveauDeVie+ "point de vie et est à pieds et mani "+armeEnMain);
+
+   return chaine_a_retourner; 
+}
+}
+
+
+
     
-    public void setCheval(boolean cheval) {
-        this.cheval = cheval;
-    }
     
-   
+
 }
