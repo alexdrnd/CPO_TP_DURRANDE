ebce81595e3250f0c6dc0115ada9f9e519b3d87a
Alexandre Durrande <alexandre.durrande@epfedu.fr>
2023-10-25 09:11:28 +0200

    La classe Cellulle lumineuse à été crée

---
+++ Mini projet/LightOff_DURRANDE_version_console/src/lightoff_durrande_version_console/LightOff_DURRANDE_version_console.java
@@ -0,0 +1,37 @@
+/*
+ * Alexandre Durrande Mini projet TDA FG2
+ */
+package lightoff_durrande_version_console;
+
+/**
+ *
+ * @author Alexandre
+ */
+public class LightOff_DURRANDE_version_console {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        
+    Cellulle_Lumineuse Case1 = new Cellulle_Lumineuse();
+    Cellulle_Lumineuse Case2 = new Cellulle_Lumineuse();
+    
+    Case1.GetEtat();
+    Case2.GetEtat();
+    System.out.println (Case1.GetEtat());
+    System.out.println (Case2.GetEtat());
+    
+    Case1.ActiverCellule();
+    
+    Case1.toString();
+    Case2.toString();
+    
+System.out.println (Case1.toString());
+System.out.println (Case2.toString());
+        
+      
+    }
+    
+}
