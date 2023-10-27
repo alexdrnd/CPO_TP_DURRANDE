8185ccff19e2f0ae957df43fe972f0fb4988d952
Alexandre Durrande <alexandre.durrande@epfedu.fr>
2023-10-27 12:55:25 +0200

    classe partie finit

--- Mini projet/LightOff_DURRANDE_version_console/src/lightoff_durrande_version_console/Partie.java
+++ Mini projet/LightOff_DURRANDE_version_console/src/lightoff_durrande_version_console/Partie.java
@@ -3,6 +3,8 @@
  */
 package lightoff_durrande_version_console;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Alexandre
@@ -16,7 +18,7 @@ package lightoff_durrande_version_console;
      *Cr?e un nouvelle grille de cellulles lumineuse pour initialis? le nombre de coup ? 0.
      */
     public Partie() {
-        grille = new GrilleDeCellulles(7, 7); 
+        grille = new GrilleDeCellulles(10, 10); 
         nbCoups = 0;
     }
 
@@ -31,27 +33,54 @@ package lightoff_durrande_version_console;
      *boucle permettant de jouer au jeu 
      */
     public void lancerPartie() {
-        System.out.println("Bienvenue dans le jeu LightOff !");
-        initialiserPartie();
-        boolean partieTerminee = false;
+        Scanner scanner = new Scanner(System.in);
+    System.out.println("Bienvenue dans le jeu LightOff !");
+    initialiserPartie();
+    boolean partieTerminee = false;
 
-        while (!partieTerminee) {
-            System.out.println(grille); // Affiche l'?tat de la grille
-            System.out.println("Nombre de coups jou?s : " + nbCoups);
+    while (!partieTerminee) {
+        System.out.println(grille); // Affiche l'?tat de la grille
+        System.out.println("Nombre de coups jou?s : " + nbCoups);
+        System.out.println("Que souhaitez-vous faire ? (1. Activer une ligne, 2. Activer une colonne, 3. Activer une diagonale, 4. Quitter) : ");
 
-            
+        int choix = scanner.nextInt();
 
-            nbCoups++;
-
-            if (grille.cellulesToutesEteintes()) {
+        switch (choix) {
+            case 1:
+                System.out.println("Num?ro de la ligne ? activer : ");
+                int ligne = scanner.nextInt();
+                grille.activerLigneDeCellules(ligne);
+                break;
+            case 2:
+                System.out.println("Num?ro de la colonne ? activer : ");
+                int colonne = scanner.nextInt();
+                grille.activerColonneDeCellules(colonne);
+                break;
+            case 3:
+                System.out.println("1. Activer diagonale descendante, 2. Activer diagonale montante : ");
+                int diagonale = scanner.nextInt();
+                if (diagonale == 1) {
+                    grille.activerDiagonaleDescendante();
+                } else {
+                    grille.activerDiagonaleMontante();
+                }
+                break;
+            case 4:
                 partieTerminee = true;
-            }
+                break;
         }
 
-        System.out.println("Bravo ! Vous avez ?teint toutes les cellules en " + nbCoups + " coups.");
-    }
+        nbCoups++;
+
+        if (grille.cellulesToutesEteintes()) {
+            partieTerminee = true;
+        }
     }
 
+    System.out.println("Bravo ! Vous avez ?teint toutes les cellules en " + nbCoups + " coups.");
+    scanner.close();
+    }
+    }
   
     
     
