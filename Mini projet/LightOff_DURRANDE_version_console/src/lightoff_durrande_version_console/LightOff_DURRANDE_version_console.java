3f39c1c41e7d6642dca61289ef52224a521469c9
Alexandre Durrande <alexandre.durrande@epfedu.fr>
2023-10-27 12:55:33 +0200

    classe partie finit

--- Mini projet/LightOff_DURRANDE_version_console/src/lightoff_durrande_version_console/LightOff_DURRANDE_version_console.java
+++ Mini projet/LightOff_DURRANDE_version_console/src/lightoff_durrande_version_console/LightOff_DURRANDE_version_console.java
@@ -13,37 +13,12 @@ public class LightOff_DURRANDE_version_console {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-    
+
+        Partie partie = new Partie();
         
-    Cellulle_Lumineuse Case1 = new Cellulle_Lumineuse();
-    Cellulle_Lumineuse Case2 = new Cellulle_Lumineuse();
-    
-    Case1.GetEtat();
-    Case2.GetEtat();
-    System.out.println (Case1.GetEtat());
-    System.out.println (Case2.GetEtat());
-    
-    Case1.ActiverCellule();
-    
-    Case1.toString();
-    Case2.toString();
-    
-System.out.println (Case1.toString());
-System.out.println (Case2.toString());
+        partie.lancerPartie();
         
-    
-
-
-
-GrilleDeCellulles Partie1 = new GrilleDeCellulles (5, 5);
-System.out.println (Partie1);
-
-
-
-
-
-
 
     }
-    
+
 }
